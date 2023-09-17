001643c71f838e16b3b0da3da0d527dc2c171ea2
vladislav-slobodenjuk <vladjun@gmail.com>
2023-09-18 00:45:54 +0300

    same

--- routes/api/auth-router.js
+++ routes/api/auth-router.js
@@ -3,11 +3,14 @@ import express from "express";
 import authController from "../../controllers/auth-controller.js";
 import validateUserRegister from "../../middlewares/validateUserRegister.js";
 import validateUserLogin from "../../middlewares/validateUserLogin.js";
+import authentificate from "../../middlewares/authentificate.js";
 
 const authRouter = express.Router();
 
 authRouter.post("/register", validateUserRegister, authController.register);
 
 authRouter.post("/login", validateUserLogin, authController.login);
+
+authRouter.get("/current", authentificate, authController.getCurrent);
 
 export default authRouter;
