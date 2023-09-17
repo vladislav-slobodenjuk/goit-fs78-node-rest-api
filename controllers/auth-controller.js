4cc0b3028efac2a700ec8d149ceb21f27e50897d
vladislav-slobodenjuk <vladjun@gmail.com>
2023-09-17 15:16:24 +0300

    hashed password

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,18 +1,25 @@
+import bcrypt from "bcrypt";
+
 import HttpError from "../helpers/HttpError.js";
 import ctrlWrapper from "../decorators/ctrlWrapper.js";
 import { User } from "../models/User.js";
 
 const register = async (req, res) => {
-  const { email } = req.body;
+  const { email, password } = req.body;
   const user = await User.findOne({ email });
 
   if (user) throw HttpError(409, `email ${email} is in use`);
 
-  const newUser = await User.create(req.body);
+  const hashedPass = await bcrypt.hash(password, 10);
+  const newUser = await User.create({ ...req.body, password: hashedPass });
 
   res.status(201).json({
     user: { email: newUser.email, subscription: newUser.subscription },
   });
+};
+
+const login = async (req, res) => {
+  //
 };
 
 export default {
